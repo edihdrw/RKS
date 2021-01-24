271788054b1ee9d3a976e8e4343e8d1b854039fa
edihdrw <edihendriawan110@gmail.com>
2021-01-25 04:22:21 +0700

    Jawaban UAS RKS

---
+++ app/src/main/java/com/example/kuncigame/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.kuncigame;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    selectedFragment = new Home();
+                    break;
+                case R.id.chat:
+                    selectedFragment = new Chat();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,selectedFragment).commit();
+            return true;
+
+        }
+    };
+}
